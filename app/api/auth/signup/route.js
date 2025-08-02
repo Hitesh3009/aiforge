b357014a4d0610a401e8150c01abae594e49c533
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-02 13:21:24 +0530

    Made this api to handle the signup of the new user

---
+++ app/api/auth/signup/route.js
@@ -0,0 +1,30 @@
+import { connectToDatabase } from '@/lib/mongodb';
+import User from '@/models/User';
+import bcrypt from 'bcryptjs';
+
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
+
+    await connectToDatabase();
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return new Response(JSON.stringify({ error: `User Exist for the email` ,status:409}), { status: 409 });
+    }
+    else{
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ name, email, password:hashedPassword });
+      await user.save();
+  
+      return new Response(JSON.stringify({ successMsg: 'User signed up successfully', user: { email: user.email, name: user.name } ,status:201}), {
+        status: 201,
+      });
+    }
+  }
+  catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500,
+    });
+  }
+}
