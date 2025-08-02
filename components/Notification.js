1a2a395505a05e4f1e421c72a92e725904d49a8d
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-02 13:25:06 +0530

    This component is used to display a Notification message to the user for successful/unsuccessful login or Signup

---
+++ components/Notification.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+function Notification({ email, message, onClose, statusCode,isLogin,userName }) {
+    useEffect(() => {
+        if (message) {
+            const timer = setTimeout(() => {
+                onClose();
+            }, 5000);
+
+            return () => clearTimeout(timer); // cleanup on unmount or if message changes
+        }
+    }, [message, onClose]);
+    if (!message) return null;
+    const isError = statusCode;
+    const textColor = isError === 409 || isError === 404 || isError === 401 ? 'text-red-700' : 'text-green-700';
+    const bgColor = isError === 409 || isError === 404 || isError === 401 ? 'bg-red-100 border-red-400' : 'bg-green-100 border-green-400';
+    return (
+        <div className="fixed top-4 right-4 z-50">
+            <div className={`border ${bgColor} ${textColor} px-4 py-3 rounded shadow-lg flex items-center justify-between gap-4`}>
+                {! isLogin ?<span>{message} <b>{email}</b></span>:<span>{message} <b>{userName}</b></span>}
+                <button onClick={onClose} className={`${textColor} font-bold`}>✖</button>
+            </div>
+        </div>
+    );
+}
+
+export default Notification;
