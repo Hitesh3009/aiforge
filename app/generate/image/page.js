3c576c0aae44f1513ac7260e24ad0626b06a0e6f
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-02 17:47:45 +0530

    This page is used to display the textarea for the user prompt and shows the generated image in gallery format.

---
+++ app/generate/image/page.js
@@ -0,0 +1,15 @@
+
+import ImageGenerationPage from '@/components/ImageGenerationPage'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function GenerateImg() {
+  return (
+    <div>
+        <Navbar/>
+        <ImageGenerationPage/>
+    </div>
+  )
+}
+
+export default GenerateImg
