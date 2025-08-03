cada046a7e56304f70c198290c4d0da18411707b
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-03 16:52:14 +0530

    Added the jwt sign to generate token for the logged in user

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,6 +1,8 @@
 import { connectToDatabase } from '@/lib/mongodb';
-import User from '@/models/User';
+import {User}  from '@/models/Schema';
+
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export async function POST(req) {
     try {
@@ -8,6 +10,9 @@ export async function POST(req) {
         console.log(email, password);
         await connectToDatabase();
         const user = await User.findOne({ email });
+        console.log(user);
+        const jwtToken=jwt.sign({id:user._id.toString(),email:user.email}, process.env.SECRET_KEY, { expiresIn: '1h'  })
+        
         if (!user) {
             return new Response(JSON.stringify({ error: 'User not found' ,status:404}), { status: 404 });
         }
@@ -18,12 +23,14 @@ export async function POST(req) {
         return new Response(JSON.stringify({
             successMsg: 'User authenticated successfully!  Welcome back!' ,
             status: 200,
+            token:jwtToken,
             user: { email: user.email, name: user.name }
         }), {
             status: 200,
         });
     }
     catch (err) {
+        console.error(err);
         return new Response(JSON.stringify({ error: err.message }), {
             status: 500,
         });
