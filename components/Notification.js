dc6150be42ec04e844962f843ebfee6ddde94e26
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-03 17:02:06 +0530

    Added more status codes

--- components/Notification.js
+++ components/Notification.js
@@ -12,12 +12,12 @@ function Notification({ email, message, onClose, statusCode,isLogin,userName }) 
     }, [message, onClose]);
     if (!message) return null;
     const isError = statusCode;
-    const textColor = isError === 409 || isError === 404 || isError === 401 ? 'text-red-700' : 'text-green-700';
-    const bgColor = isError === 409 || isError === 404 || isError === 401 ? 'bg-red-100 border-red-400' : 'bg-green-100 border-green-400';
+    const textColor = isError === 409 || isError === 404 || isError === 401 || isError === 500 ? 'text-red-700' : 'text-green-700';
+    const bgColor = isError === 409 || isError === 404 || isError === 401 || isError === 500 ? 'bg-red-100 border-red-400' : 'bg-green-100 border-green-400';
     return (
         <div className="fixed top-4 right-4 z-50">
             <div className={`border ${bgColor} ${textColor} px-4 py-3 rounded shadow-lg flex items-center justify-between gap-4`}>
-                {! isLogin ?<span>{message} <b>{email}</b></span>:<span>{message} <b>{userName}</b></span>}
+                {! isLogin ?<span>{message}</span>:<span>{message} <b>{userName}</b></span>}
                 <button onClick={onClose} className={`${textColor} font-bold`}>✖</button>
             </div>
         </div>
