d95db9a4687ed6e5830bd374d0786ee001f36853
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-03 16:55:10 +0530

    This api is now secure by the JWT authorization token

--- app/api/generate/image/route.js
+++ app/api/generate/image/route.js
@@ -2,35 +2,54 @@ import { GoogleGenAI, Modality } from "@google/genai";
 
 export async function POST(req) {
     const { prompt } = await req.json();
-    try {
-        const apiKey = process.env.GOOGLE_API_KEY
-        const ai = new GoogleGenAI({
-            apiKey: apiKey
-        });
-        const contents = `${prompt}`;
-        const imagePromises = Array.from({ length: 6 }, async () => {
-            return await ai.models.generateContent({
-                model: "gemini-2.0-flash-preview-image-generation",
-                contents: contents,
-                config: {
-                    responseModalities: [Modality.TEXT, Modality.IMAGE],
-                },
-            });
-        });
-        const resolvedPromiseRes = await Promise.all(imagePromises);
-
-        const responseArr = resolvedPromiseRes.map((response) => {
-            const imagePart = response.candidates[0].content.parts.find(part => part.inlineData);
-            if (imagePart) {
-                const base64Encode = imagePart.inlineData.data;
-                return base64Encode
-            }
-            setTimeout(()=>{},23000)
-        });
-        // console.log('Generated images:', responseArr);
-        return new Response(JSON.stringify({ images: responseArr, status: 201 }, { status: 201 }));
+    const authHeader = await req.headers.get('authorization');;
+    console.log(authHeader);
+    
+    if (!authHeader) {
+        return new Response(JSON.stringify({ error: 'Authorization token is required' }, { status: 401 }));
     }
-    catch (error)  {
+    const token = authHeader.split(' ')[1];
+    try {
+        const verifyPayload = await fetch('http://localhost:3000/api/auth/verify', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ token: token })
+        });
+
+        if (!verifyPayload.ok) {
+            return new Response(JSON.stringify({ error: 'Unauthorized' }, { status: 401 }));
+        }
+        else {
+
+            const apiKey = process.env.GOOGLE_API_KEY
+            const ai = new GoogleGenAI({
+                apiKey: apiKey
+            });
+            const contents = `${prompt}`;
+            const imagePromises = Array.from({ length: 6 }, async () => {
+
+                return await ai.models.generateContent({
+                    model: "gemini-2.0-flash-preview-image-generation",
+                    contents: contents,
+                    config: {
+                        responseModalities: [Modality.TEXT, Modality.IMAGE],
+                    }
+                });
+            });
+            const resolvedPromiseRes = await Promise.all(imagePromises);
+
+            const responseArr = resolvedPromiseRes.map((response) => {
+                const imagePart = response.candidates[0].content.parts.find(part => part.inlineData);
+                if (imagePart) {
+                    const base64Encode = imagePart.inlineData.data;
+                    return base64Encode
+                }
+            });
+            // console.log('Generated images:', responseArr);
+            return new Response(JSON.stringify({ images: responseArr, status: 201 , userDetails:await verifyPayload.json()}, { status: 201 }));
+        }
+    }
+    catch (error) {
         console.error(error);
         return new Response(JSON.stringify({ error: 'Internal Server Error' },
             { status: 500 }
