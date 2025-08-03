3ca84ad39d010c43876cec778a3deaadfd19e47b
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-03 16:57:09 +0530

    Added the Navbar component

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,9 +1,11 @@
+import Navbar from '@/components/Navbar'
 import Signup from '@/components/Signup'
 import React from 'react'
 
 function SignupPage() {
   return (
     <div>
+      <Navbar/>
         <Signup/>
     </div>
   )
