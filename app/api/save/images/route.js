6210cf416f2228144aaaf57decd64b87c63fe6a3
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-08 22:13:48 +0530

    Change the login again logic and change the image from array data type to object

--- app/api/save/images/route.js
+++ app/api/save/images/route.js
@@ -2,7 +2,6 @@ import { connectToDatabase } from '@/lib/mongodb';
 import { Image } from '@/models/Schema';
 export async function POST(req) {
     const authHeader = await req.headers.get('authorization');;
-    console.log(authHeader);
     
     if (!authHeader) {
         return new Response(JSON.stringify({ error: 'Authorization token is required' ,status:401}, { status: 401 }));
@@ -16,21 +15,19 @@ export async function POST(req) {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ token: tokenFromHeader })
         });
-        if(verifyPayload.ok) {
-            const verifyResult = await verifyPayload.json();
-            console.log(verifyResult);
-            
-            const userImageGallery = new Image({
-                'userId': verifyResult.userID, 'imgDesc': prompt, 'images': selectedImages.map((img) => {
-                    let imageBuffer = Buffer.from(img, 'base64');
-                    return { contentType: 'image/png', data: imageBuffer };
-                }), createdAt: Date.now()
-            });
-            await userImageGallery.save();
+        const verifyResult = await verifyPayload.json();
+        console.log(verifyResult);
+        if(verifyResult.success) {
+            selectedImages.forEach(async(img)=>{    
+                const userImageGallery = new Image({
+                    'userId': verifyResult.userID, 'imgDesc': prompt, 'images': {data:Buffer.from(img, 'base64'),contentType: 'image/png'}, createdAt: Date.now()
+                });
+                await userImageGallery.save();
+            })
             return new Response(JSON.stringify({ message: 'Images saved successfully' ,status:200}), { status: 200 });
         }
         else{
-            return new Response(JSON.stringify({ error: verifyResult.error + ' Please,Login Again.' ,status:401}), { status: 401 });
+            return new Response(JSON.stringify({ error: 'Unauthorized.Please,Login Again.' ,status:401}), { status: 401 });
         }
     } catch (error) {
         console.error(error);
