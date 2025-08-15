c3f334f95873ed7f39e3862027f297b3dd3a6b06
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-08-16 01:46:03 +0530

    component to display the input file option for editing image

---
+++ app/edit/image/page.js
@@ -0,0 +1,14 @@
+import EditImage from '@/components/EditImage'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function EditUserUploadedImg() {
+  return (
+    <div>
+        <Navbar/>
+        <EditImage/>
+    </div>
+  )
+}
+
+export default EditUserUploadedImg
