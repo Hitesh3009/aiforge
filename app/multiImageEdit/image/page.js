fa460f891423eef31968c5419caf7a06d0dc8f03
Hitesh3009 <hiteshbhosle3009@gmail.com>
2025-09-02 23:14:31 +0530

    This page displays the EditUsingMultipleImages component

---
+++ app/multiImageEdit/image/page.js
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import EditUsingMultipleImages from '@/components/EditUsingMultipleImages'
+import React from 'react'
+
+function MultiImageEdit() {
+  return (
+    <div>
+        <Navbar/>
+        <EditUsingMultipleImages/>
+    </div>
+  )
+}
+
+export default MultiImageEdit
