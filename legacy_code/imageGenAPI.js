a1aebaff07e82e2a368581e642a7353c05029807
Hitesh3009 <hiteshbhosle3009@gmail.com>
2026-02-22 21:40:45 +0530

    legacy code for api

---
+++ legacy_code/imageGenAPI.js
@@ -0,0 +1,65 @@
+import { GoogleGenAI, Modality } from "@google/genai";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '100mb',
+    },
+  },
+};
+
+
+export async function POST(req) {
+    const { prompt } = await req.json();
+    const session = await getServerSession(authOptions);
+
+    try {
+        if (session) {
+            const apiKey = process.env.GOOGLE_API_KEY
+            console.log(apiKey);
+            
+            const ai = new GoogleGenAI({
+                apiKey: apiKey
+            });
+
+            // console.log(ai);
+            
+            const contents = `${prompt}`;
+            const imagePromises = Array.from({ length: 6 }, async () => {
+
+                return await ai.models.generateContent({
+                    model: "gemini-2.0-flash-preview-image-generation",
+                    contents: contents,
+                    config: {
+                        responseModalities: [Modality.TEXT, Modality.IMAGE],
+                    }
+                });
+            });
+            const resolvedPromiseRes = await Promise.all(imagePromises);
+
+            const responseArr = resolvedPromiseRes.map((response) => {
+                const imagePart = response.candidates[0].content.parts.find(part => part.inlineData);
+                if (imagePart) {
+                    const base64Encode = imagePart.inlineData.data;
+                    return base64Encode
+                }
+            });
+            // console.log('Generated images:', responseArr);
+            return new Response(JSON.stringify({ images: responseArr, status: 201 }, { status: 201 }));
+        }
+        else {
+            return new Response(
+                JSON.stringify({ error: "Unauthorized", success: false }),
+                { status: 401 }
+            );
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' },
+            { status: 500 }
+        ))
+    }
+}
---
+++ legacy_code/multiImageEditAPI.js
@@ -0,0 +1,70 @@
+import { GoogleGenAI, Modality } from "@google/genai";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export async function POST(req) {
+    const session = await getServerSession(authOptions);
+    try {
+        
+        if (session) {
+            const { prompt,referenceImg,targetImg } = await req.json();
+            const apiKey = process.env.GOOGLE_API_KEY
+            const ai = new GoogleGenAI({
+                apiKey: apiKey
+            });
+            
+            const base64Image1 = referenceImg ;
+            const base64Image2 = targetImg ;
+            
+            
+            const contents = [
+                { text: `${prompt}` },
+                {
+                    inlineData: {
+                        mimeType: "image/png",
+                        data: base64Image1,
+                    }
+                },
+                {
+                    inlineData: {
+                        mimeType: "image/png",
+                        data: base64Image2,
+                    }
+                }
+            ];
+            // console.log(contents);
+            
+            const imagePromises = Array.from({ length: 3 }, async () => {
+                
+                return await ai.models.generateContent({
+                    model: "gemini-2.0-flash-preview-image-generation",
+                    contents: contents,
+                    config: {
+                        responseModalities: [Modality.TEXT, Modality.IMAGE,Modality.IMAGE],
+                    },
+                })
+            });
+            const resolvedPromiseRes = await Promise.all(imagePromises);
+            // console.log(resolvedPromiseRes[0].candidates[0].content.parts);
+            
+            const responseArr = resolvedPromiseRes.map((response) => {
+                const imagePart = response.candidates[0].content.parts.find(part => part.inlineData);
+                if (imagePart) {
+                    const base64Encode = imagePart.inlineData.data;
+                    return base64Encode
+                }
+            });
+            // console.log('Generated images:', responseArr);
+            return new Response(JSON.stringify({ images: responseArr, status: 201 }, { status: 201 }));
+        }
+        else {
+            return new Response(JSON.stringify({ error: 'Unauthorized' }, { status: 401 }));
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' },
+            { status: 500 }
+        ))
+    }
+}
