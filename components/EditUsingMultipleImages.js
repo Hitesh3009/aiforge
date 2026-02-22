fe2d879277d6523f953a772119ebbfbc3841d118
Hitesh3009 <hiteshbhosle3009@gmail.com>
2026-02-22 21:40:16 +0530

    replace base64 with output image url

--- components/EditUsingMultipleImages.js
+++ components/EditUsingMultipleImages.js
@@ -191,6 +191,7 @@ export default function EditWithReference() {
                             onChange={(e) => handleFileUpload(e, "target")}
                         />
                     </label>
+
                 </div>
 
                 {/* Previews */}
@@ -335,7 +336,7 @@ export default function EditWithReference() {
                                         }`}
                                 >
                                     <img
-                                        src={`data:image/png;base64,${img}`}
+                                        src={`${img}`}
                                         alt={`Edited ${index}`}
                                         className="rounded-lg max-h-64 object-cover w-full h-full"
                                     />
@@ -343,7 +344,7 @@ export default function EditWithReference() {
                                     <button
                                         onClick={(e) => {
                                             e.stopPropagation();
-                                            setZoomedImage(`data:image/png;base64,${img}`);
+                                            setZoomedImage(`${img}`);
                                         }}
                                         className="absolute top-2 right-2 bg-white bg-opacity-80 rounded-full p-2 shadow hover:bg-opacity-100"
                                     >
