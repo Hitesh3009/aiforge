316212ed332ba2ffff5fe60858de7afa7eba3f73
Hitesh3009 <hiteshbhosle3009@gmail.com>
2026-02-22 21:38:06 +0530

    Replaced the google gemini api with freepik api for editing images

--- app/api/edit/image/route.js
+++ app/api/edit/image/route.js
@@ -1,57 +1,90 @@
-import { GoogleGenAI, Modality } from "@google/genai";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '100mb',
+        },
+    },
+};
+
+
 export async function POST(req) {
     const session = await getServerSession(authOptions);
+    
     try {
-        
         if (session) {
             const { prompt,inpBase64Img } = await req.json();
-            const apiKey = process.env.GOOGLE_API_KEY
-            const ai = new GoogleGenAI({
-                apiKey: apiKey
-            });
-            
-            const base64Image = inpBase64Img ;
-            
-            
-            const contents = [
-                { text: `${prompt}` },
-                {
-                    inlineData: {
-                        mimeType: "image/png",
-                        data: base64Image,
-                    }
-                },
-            ];
-            // console.log(contents);
-            
+            const apiKey = process.env.FREEPIK_API_KEY
+            //console.log(apiKey);
+
+            const url = 'https://api.freepik.com/v1/ai/text-to-image/seedream-v4-5-edit';
+            const options = {
+                method: 'POST',
+                headers: { 'x-freepik-api-key': apiKey, 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "prompt": prompt,"reference_images": [inpBase64Img],"enable_safety_checker":false ,"aspect_ratio": "square_1_1", "seed": 1073741823 })
+            };
+
+            //console.log(options);
+
             const imagePromises = Array.from({ length: 3 }, async () => {
-                
-                return await ai.models.generateContent({
-                    model: "gemini-2.0-flash-preview-image-generation",
-                    contents: contents,
-                    config: {
-                        responseModalities: [Modality.TEXT, Modality.IMAGE],
-                    },
-                })
+                const response = await fetch(url, options);
+                const data = await response.json();
+                // console.log('what is data', data);
+
+                return data.data;
             });
+
+            //console.log(imagePromises);
             const resolvedPromiseRes = await Promise.all(imagePromises);
-            // console.log(resolvedPromiseRes[0].candidates[0].content.parts);
-            
-            const responseArr = resolvedPromiseRes.map((response) => {
-                const imagePart = response.candidates[0].content.parts.find(part => part.inlineData);
-                if (imagePart) {
-                    const base64Encode = imagePart.inlineData.data;
-                    return base64Encode
+            //console.log(resolvedPromiseRes);
+
+            let finalImageArr = [];
+
+            const taskIds = resolvedPromiseRes.map(item => item.task_id);
+
+            let status = "IN_PROGRESS";
+            while (status !== "COMPLETED") {
+                await new Promise((res) => setTimeout(res, 3000));
+
+                const pollResults = await Promise.all(
+                    taskIds.map(async (taskId) => {
+                        const pollRes = await fetch(
+                            `https://api.freepik.com/v1/ai/text-to-image/seedream-v4-5-edit/${taskId}`,
+                            {
+                                method: "GET",
+                                headers: { "x-freepik-api-key": apiKey }
+                            }
+                        );
+                        return pollRes.json();
+                    })
+                );
+
+                // Check if all tasks are completed
+                status = pollResults.every(r => r.data.status === "COMPLETED")
+                    ? "COMPLETED"
+                    : "IN_PROGRESS";
+
+                if (status === "COMPLETED") {
+                    pollResults.forEach(r => {
+                        if (r.data.generated) {
+                            finalImageArr.push(r.data.generated[0]);
+                        }
+                    });
                 }
-            });
-            // console.log('Generated images:', responseArr);
-            return new Response(JSON.stringify({ images: responseArr, status: 201 }, { status: 201 }));
+            }
+
+
+            //console.log(finalImageArr);
+
+            return new Response(JSON.stringify({ images: finalImageArr, status: 201 }, { status: 201 }));
         }
         else {
-            return new Response(JSON.stringify({ error: 'Unauthorized' }, { status: 401 }));
+            return new Response(
+                JSON.stringify({ error: "Unauthorized", success: false }),
+                { status: 401 }
+            );
         }
     }
     catch (error) {
